a667bf0442e85655abd1532db31b1448f6c2fff6
Jiqing Yao <jqyao@yahoo-inc.com>
2011-02-15 09:55:01 -0500

    add perl

---
+++ perl/getopt.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl -w
+
+use strict;
+use Data::Dumper;
+use Log::Log4perl qw(:easy);
+
+use Getopt::Long ;
+
+my %opts ;
+
+
+my $res = GetOptions( \%opts , 'debug|d' , 'verbose|v' );
+if( $opts{debug} )
+{
+    Log::Log4perl->easy_init($DEBUG);
+}
+elsif( $opts{verbose})
+{
+    Log::Log4perl->easy_init($ERROR);
+}
+
+DEBUG Dumper( \%opts );
