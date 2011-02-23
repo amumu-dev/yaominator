a05cd178ce2bd5bb8c1588524cd53563a3e9823a
Jiqing Yao <jqyao@yahoo-inc.com>
2011-02-23 15:14:31 -0500

    test

--- scripts/bash/getopt.sh
+++ scripts/bash/getopt.sh
@@ -13,3 +13,21 @@ for i
 do
   echo "-->$i"
 done
+
+for i
+do
+    case "$i"
+        in
+        -a|-b)
+        echo flag $i set; sflags="${i#-}$sflags";
+        shift;;
+        -o)
+        echo oarg is "'"$2"'"; oarg="$2"; shift;
+        shift;;
+        --)
+        shift; break;;
+    esac
+done
+echo single-char flags: "'"$sflags"'"
+echo oarg is "'"$oarg"'"
+
---
+++ scripts/bash/test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+
+
+#tmpfile=/tmp/$$.test
+#
+#ls -l &> $tmpfile 
+#
+#cat $tmpfile | grep test
+#
+#rm -rf $tmpfile
+
+hostgroup=fe
+
+echo ${hostgroup}1
