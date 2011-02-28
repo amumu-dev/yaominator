a60dbc5068df6ee02d3ce5418d5f3d2220d1c408
Jiqing Yao <jqyao@yahoo-inc.com>
2011-02-28 15:49:00 -0500

    more change

--- scripts/perl/yaml.pl
+++ scripts/perl/yaml.pl
@@ -3,12 +3,37 @@
 
 use strict;
 
+#use YAML::Syck;
 use YAML;
+use Data::Dumper;
 
-my $data = { new => [ 'ar1' , 'ar2' , 'ar3' , [ 'in1' , 'in2']] , old => { inner=> 1 }};
+#my $data = { new => [ 'ar1' , 'ar2' , 'ar3' , [ 'in1' , 'in2']] , old => { inner=> 1 }};
 
-print Dump($data);
+#print Dump($data);
 
-my @array = qw( 1 3 5 7 9);
-print Dump($data , \@array);
+#my @array = qw( 1 3 5 7 9);
+#print Dump($data , \@array);
 
+my $data;
+my $yaml;
+{
+    local $/=undef;
+    $yaml= <DATA>;
+    $data = Load($yaml) ;
+}
+
+print Dumper($data);
+=pod
+colo:
+  corp: test
+  sp2: more 
+env:
+  prod:
+  sandbox:
+host:
+=cut
+
+#[colo: prod, env: sandbox]
+__DATA__
+settings: 
+   - test: [colo: prod]
