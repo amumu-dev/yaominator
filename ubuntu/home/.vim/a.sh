91c60a4492d0d4b160d3fbcffc272b3d9065b269
Jiqing Yao <jqyao@yahoo-inc.com>
2011-02-28 15:25:28 -0500

    add bash support

---
+++ ubuntu/home/.vim/a.sh
@@ -0,0 +1,22 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE:  a.sh
+# 
+#         USAGE:  ./a.sh 
+# 
+#   DESCRIPTION:  
+# 
+#       OPTIONS:  ---
+#  REQUIREMENTS:  ---
+#          BUGS:  ---
+#         NOTES:  ---
+#        AUTHOR: YOUR NAME (), 
+#       COMPANY: 
+#       CREATED: 02/28/2011 15:25:07 EST
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+
+
